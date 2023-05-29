c9f598c52a38b0d63259a07669ee2779bfe4241e
GlazHorsez <to5gogol@gmail.com>
2023-05-29 13:15:53 +0300

    added task n19

---
+++ task19/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("insert an integer: ");
+string number=Console.ReadLine()!;
+bool comp=true;
+for(int i=number.Length-1;i>=number.Length/2;i--){
+    if (number[number.Length-i-1]!=number[i]){
+        comp=false;
+        break;
+    }
+}
+if (comp)Console.WriteLine($"The number {number} is a polyndrome");
+else Console.WriteLine($"The number {number} is not a polyndrome");
