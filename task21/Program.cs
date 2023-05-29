98df8e8cd927008dbd22e34dc75c968810050f0d
GlazHorsez <to5gogol@gmail.com>
2023-05-29 14:11:28 +0300

    added task n21

---
+++ task21/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.Write("insert coordinates of A dot: ");
+int [] a=new int [3];
+Console.Write("x: ");
+a[0]= int.Parse(Console.ReadLine()!);
+Console.Write("y: ");
+a[1]= int.Parse(Console.ReadLine()!);
+Console.Write("z: ");
+a[2]= int.Parse(Console.ReadLine()!);
+
+Console.Write("insert coordinates of B dot: ");
+int [] b=new int [3];
+Console.Write("x: ");
+b[0]= int.Parse(Console.ReadLine()!);
+Console.Write("y: ");
+b[1]= int.Parse(Console.ReadLine()!);
+Console.Write("z: ");
+b[2]= int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"distance between A and B dots equal {Math.Sqrt(Math.Sqrt(Math.Pow((a[0]-b[0]),2)+Math.Pow((a[1]-b[1]),2))+Math.Pow((a[2]-b[2]),2))} ");
